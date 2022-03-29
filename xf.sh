44193e561f66655093bb0fe0bc65e831c072db28
gy86 <516888@live.com>
2022-03-29 11:28:26 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "20d1b4ab-c481-48e1-b79e-5b6a4a32f712",
                         "alterId": 0
                     }
                 ],
